99e7416910b8fd57cbbc18407a56b749f2c8eada
Ilya Tyamin <tyaminilya@gmail.com>
2024-06-09 00:45:19 +0300

    Auth-Service: 50/50 Docker compose support (I am trying)

---
+++ auth-service/src/main/resources/db/changelog/changeset/fill-users.sql
@@ -0,0 +1,4 @@
+INSERT INTO "user" (created, email, password, nickname)
+VALUES ('2024-06-06 10:00:00', 'testemail@email.ru', 'RealGenious1!', 'jimmiNeytron'),
+       ('2024-06-06 11:00:00', 'another@gmail.ru', 'WowMyPassword!', 'somebody'),
+       ('2024-05-05 23:59:46', 'hse@hse.ru', 'HSEStudent2024!!!', 'IamHSEStudent')
