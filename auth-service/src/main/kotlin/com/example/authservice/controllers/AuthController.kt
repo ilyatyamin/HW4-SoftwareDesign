4b044a100a13731439f2db00f8de78ea0f6fc0c5
Ilya Tyamin <tyaminilya@gmail.com>
2024-06-09 13:57:08 +0300

    Auth-Service: add documentation (Postman examples)

--- auth-service/src/main/kotlin/com/example/authservice/controllers/AuthController.kt
+++ auth-service/src/main/kotlin/com/example/authservice/controllers/AuthController.kt
@@ -2,30 +2,63 @@ package com.example.authservice.controllers
 
 import com.example.authservice.dto.*
 import com.example.authservice.services.AuthService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import io.swagger.v3.oas.annotations.parameters.RequestBody
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/auth")
 class AuthController(private val authService: AuthService) {
     @PostMapping("/register")
-    fun register(@RequestBody request: RegistrationRequest): ResponseEntity<RegistrationResponse> {
+    @Operation(summary = "Register user if all data is correctly filled.")
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "Successfully add user to database"),
+            ApiResponse(responseCode = "400", description = "The user entered incorrect data: the password or email address did not arrive or entered an incorrect form (one of the fields is null)"),
+            ApiResponse(responseCode = "409", description = "User with this data already existed in system.")
+        ])
+    fun register(@RequestBody(content = [Content(schema = Schema(implementation = RegistrationRequest::class))])
+                 request: RegistrationRequest): ResponseEntity<RegistrationResponse> {
         val answer = authService.registerUser(request)
         return ResponseEntity.status(answer.status).body(answer.info)
     }
 
+
+
     @PostMapping("/login")
-    fun login(@RequestBody request : AuthRequest) : ResponseEntity<AuthResponse> {
+    @Operation(summary = "Login user and send back user's token")
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "The user is in the database, everything is correct"),
+            ApiResponse(responseCode = "400", description = "The user entered incorrect data: one of the fields possible null"),
+            ApiResponse(responseCode = "403", description = "User with this data is not in the database.")
+        ])
+    fun login(@RequestBody(content = [Content(schema = Schema(implementation = AuthRequest::class))])
+              request: AuthRequest): ResponseEntity<AuthResponse> {
         val answer = authService.tryToLogin(request)
         return ResponseEntity.status(answer.status).body(answer.info)
     }
 
+
+
     @PostMapping("/getInfo")
-    fun getInfo(@RequestBody request: InfoRequest) : ResponseEntity<InfoResponse> {
+    @Operation(summary = "Get info about user by token")
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "Everything is OK"),
+            ApiResponse(responseCode = "403", description = "The received token does not exist")
+        ])
+    fun getInfo(@RequestBody(content = [Content(schema = Schema(implementation = InfoRequest::class))])
+                request: InfoRequest): ResponseEntity<InfoResponse> {
         val answer = authService.getInfo(request)
         return ResponseEntity.status(answer.status).body(answer.info)
     }
+
 }
--- auth-service/src/main/kotlin/com/example/authservice/dto/RegistrationRequest.kt
+++ auth-service/src/main/kotlin/com/example/authservice/dto/RegistrationRequest.kt
@@ -1,7 +1,22 @@
 package com.example.authservice.dto
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 data class RegistrationRequest(
-    var nickname : String?,
-    var email : String?,
-    var password : String?
+    @Schema(example = "my_nickname", nullable = false)
+    var nickname: String?,
+
+    @Schema(
+        example = "myemail@gmail.com",
+        nullable = false,
+        format = "must contains @ and . and be at least 5 symbols in size"
+    )
+    var email: String?,
+
+    @Schema(
+        example = "reallyGood12345!",
+        nullable = false,
+        format = "must contains upper and lower symbols, numbers at be at least 8 symbols in size "
+    )
+    var password: String?
 )
